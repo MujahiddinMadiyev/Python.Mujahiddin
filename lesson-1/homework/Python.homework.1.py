0d474f9f035caf0812ff068e8fe0b6fc71da722e
MujahiddinMadiyev <official.mr.mujahiddin@gmail.com>
2025-04-09 15:11:35 +0500

    Add files via upload

---
+++ lesson-1/homework/Python.homework.1.py
@@ -0,0 +1,164 @@
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "id": "12c42b8d",
+   "metadata": {},
+   "source": [
+    "# Homework Python lesson 1"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "dfeec215",
+   "metadata": {},
+   "source": [
+    "### 1. Given a side of square. Find its perimeter and area."
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 9,
+   "id": "5501ea66",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "16\n",
+      "16\n"
+     ]
+    }
+   ],
+   "source": [
+    "side = 4\n",
+    "perimeter = 4 * side\n",
+    "area = side ** 2\n",
+    "\n",
+    "print(perimeter)\n",
+    "print(area)"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "a91c7dfe",
+   "metadata": {},
+   "source": [
+    "### 2. Given diameter of circle. Find its length."
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 8,
+   "id": "34e436fb",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "30\n"
+     ]
+    }
+   ],
+   "source": [
+    "diameter = 10\n",
+    "length = 3 * diameter\n",
+    "\n",
+    "print(length)"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "99515765",
+   "metadata": {},
+   "source": [
+    "### 3. Given two numbers a and b. Find their mean."
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 10,
+   "id": "4a2c5396",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "15.0\n"
+     ]
+    }
+   ],
+   "source": [
+    "a = 10\n",
+    "b = 20\n",
+    "\n",
+    "mean = (a + b) / 2\n",
+    "print(mean)"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "3cf0b0ed",
+   "metadata": {},
+   "source": [
+    "### 4. Given two numbers a and b. Find their sum, product and square of each number."
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 12,
+   "id": "3ef48d9a",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "8\n",
+      "15\n",
+      "25\n",
+      "9\n"
+     ]
+    }
+   ],
+   "source": [
+    "a = 5  \n",
+    "b = 3 \n",
+    "\n",
+    "sum_result = a + b\n",
+    "product_result = a * b\n",
+    "square_a = a ** 2\n",
+    "square_b = b ** 2\n",
+    "\n",
+    "print(sum_result)\n",
+    "print(product_result)\n",
+    "print(square_a)\n",
+    "print(square_b)"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.9"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
